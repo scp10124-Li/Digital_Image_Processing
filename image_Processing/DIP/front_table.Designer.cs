110a6bf572934b29814e4564daf6b3bcdf792b11
scp10124 <aa0970543252@gmail.com>
2025-03-15 12:48:18 +0800

    initial commit and add image Rotation

---
+++ image_Processing/DIP/front_table.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace DIP
+{
+    partial class front_table
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.stStripLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rGBtoGrayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verticalFlipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.horizontalFlipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.degreeRotationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.degreeRotationToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.oFileDlg = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stStripLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 503);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(827, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // stStripLabel
+            // 
+            this.stStripLabel.Name = "stStripLabel";
+            this.stStripLabel.Size = new System.Drawing.Size(128, 17);
+            this.stStripLabel.Text = "toolStripStatusLabel1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.iPToolStripMenuItem,
+            this.imageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(827, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // iPToolStripMenuItem
+            // 
+            this.iPToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rGBtoGrayToolStripMenuItem});
+            this.iPToolStripMenuItem.Name = "iPToolStripMenuItem";
+            this.iPToolStripMenuItem.Size = new System.Drawing.Size(29, 20);
+            this.iPToolStripMenuItem.Text = "&IP";
+            this.iPToolStripMenuItem.Click += new System.EventHandler(this.iPToolStripMenuItem_Click);
+            // 
+            // rGBtoGrayToolStripMenuItem
+            // 
+            this.rGBtoGrayToolStripMenuItem.Name = "rGBtoGrayToolStripMenuItem";
+            this.rGBtoGrayToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rGBtoGrayToolStripMenuItem.Text = "RGBtoGray";
+            this.rGBtoGrayToolStripMenuItem.Click += new System.EventHandler(this.rGBtoGrayToolStripMenuItem_Click);
+            // 
+            // imageToolStripMenuItem
+            // 
+            this.imageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verticalFlipToolStripMenuItem,
+            this.horizontalFlipToolStripMenuItem,
+            this.degreeRotationToolStripMenuItem,
+            this.degreeRotationToolStripMenuItem1});
+            this.imageToolStripMenuItem.Name = "imageToolStripMenuItem";
+            this.imageToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.imageToolStripMenuItem.Text = "&image";
+            // 
+            // verticalFlipToolStripMenuItem
+            // 
+            this.verticalFlipToolStripMenuItem.Name = "verticalFlipToolStripMenuItem";
+            this.verticalFlipToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.verticalFlipToolStripMenuItem.Text = "Vertical Flip";
+            this.verticalFlipToolStripMenuItem.Click += new System.EventHandler(this.verticalFlipToolStripMenuItem_Click);
+            // 
+            // horizontalFlipToolStripMenuItem
+            // 
+            this.horizontalFlipToolStripMenuItem.Name = "horizontalFlipToolStripMenuItem";
+            this.horizontalFlipToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.horizontalFlipToolStripMenuItem.Text = "Horizontal Flip";
+            this.horizontalFlipToolStripMenuItem.Click += new System.EventHandler(this.horizontalFlipToolStripMenuItem_Click);
+            // 
+            // degreeRotationToolStripMenuItem
+            // 
+            this.degreeRotationToolStripMenuItem.Name = "degreeRotationToolStripMenuItem";
+            this.degreeRotationToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.degreeRotationToolStripMenuItem.Text = "Counterclockwise_Rotation";
+            this.degreeRotationToolStripMenuItem.Click += new System.EventHandler(this.degreeRotationToolStripMenuItem_Click_1);
+            // 
+            // degreeRotationToolStripMenuItem1
+            // 
+            this.degreeRotationToolStripMenuItem1.Name = "degreeRotationToolStripMenuItem1";
+            this.degreeRotationToolStripMenuItem1.Size = new System.Drawing.Size(226, 22);
+            this.degreeRotationToolStripMenuItem1.Text = "clockwise_Rotation";
+            this.degreeRotationToolStripMenuItem1.Click += new System.EventHandler(this.degreeRotationToolStripMenuItem1_Click_1);
+            // 
+            // oFileDlg
+            // 
+            this.oFileDlg.FileName = "openFileDialog1";
+            // 
+            // front_table
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 525);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "front_table";
+            this.Text = "B11217059";
+            this.Load += new System.EventHandler(this.DIPSample_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel stStripLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iPToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog oFileDlg;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem rGBtoGrayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verticalFlipToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem horizontalFlipToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem degreeRotationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem degreeRotationToolStripMenuItem1;
+    }
+}
---
+++ image_Processing/DIP/front_table.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+
+
+namespace DIP
+{
+
+    public partial class front_table : Form
+    {
+        public front_table()
+        {
+            InitializeComponent();
+        }
+        [DllImport("togray.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe static extern void toGray(int* f0, int w, int h, int* g0);
+        [DllImport("direction.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe static extern void Vertical_Flip(int* f0, int w, int h, int* g0);
+        [DllImport("direction.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe static extern void Horizontal_Flip(int* f0, int w, int h, int* g0);
+        [DllImport("direction.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe static extern void clockwise_Rotation(int* f0, int w, int h, int* g0);
+        [DllImport("direction.dll", CallingConvention = CallingConvention.Cdecl)]
+        unsafe static extern void Counterclockwise_Rotation(int* f0, int w, int h, int* g0);
+
+        Bitmap NpBitmap;
+        int[] f;
+        int[] g;
+        int w, h;
+
+        private void DIPSample_Load(object sender, EventArgs e)
+        {
+            this.IsMdiContainer = true;
+            this.WindowState = FormWindowState.Maximized;
+			this.stStripLabel.Text = "";
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            oFileDlg.CheckFileExists = true;
+            oFileDlg.CheckPathExists = true;
+            oFileDlg.Title = "Open File - DIP Sample";
+            oFileDlg.ValidateNames = true;
+            oFileDlg.Filter = "bmp files (*.bmp)|*.bmp";
+            oFileDlg.FileName = "";
+
+            if (oFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                MSForm childForm = new MSForm();
+                childForm.MdiParent = this;
+                childForm.pf1 = stStripLabel;
+                NpBitmap = bmp_read(oFileDlg);
+                childForm.pBitmap = NpBitmap;
+                w = NpBitmap.Width;
+                h = NpBitmap.Height;
+                childForm.Show();
+            }
+        }
+
+        private Bitmap bmp_read(OpenFileDialog oFileDlg)
+        {
+            Bitmap pBitmap;
+            string fileloc = oFileDlg.FileName;
+            pBitmap = new Bitmap(fileloc);
+            w = pBitmap.Width;
+            h = pBitmap.Height;
+            return pBitmap;
+        }
+
+        private int[] bmp2array(Bitmap myBitmap)
+        {
+            int[] ImgData = new int[myBitmap.Width * myBitmap.Height];
+            BitmapData byteArray = myBitmap.LockBits(new Rectangle(0, 0, myBitmap.Width, myBitmap.Height),
+                                          ImageLockMode.ReadWrite,
+                                          myBitmap.PixelFormat);
+            int ByteOfSkip = byteArray.Stride - byteArray.Width * (int)(byteArray.Stride / myBitmap.Width);
+            unsafe
+            {
+                byte* imgPtr = (byte*)(byteArray.Scan0);
+                for (int y = 0; y < byteArray.Height; y++)
+                {
+                    for (int x = 0; x < byteArray.Width; x++)
+                    {
+                        ImgData[x + byteArray.Height * y] = (int)*(imgPtr);
+                        //ImgData[x, y] = (int)*(imgPtr + 1);
+                        //ImgData[x, y] = (int)*(imgPtr + 2);
+                        imgPtr += (int)(byteArray.Stride / myBitmap.Width);
+                    }
+                    imgPtr += ByteOfSkip;
+                }
+            }
+            myBitmap.UnlockBits(byteArray);
+            return ImgData;
+        }
+
+        private static Bitmap array2bmp(int[] ImgData)
+        {
+            int Width = (int)Math.Sqrt(ImgData.GetLength(0));
+            int Height = (int)Math.Sqrt(ImgData.GetLength(0));
+            Bitmap myBitmap = new Bitmap(Width, Height, PixelFormat.Format24bppRgb);
+            BitmapData byteArray = myBitmap.LockBits(new Rectangle(0, 0, Width, Height),
+                                           ImageLockMode.WriteOnly,
+                                           PixelFormat.Format24bppRgb);
+            //Padding bytes的長度
+            int ByteOfSkip = byteArray.Stride - myBitmap.Width * 3;
+            unsafe
+            {                                   // 指標取出影像資料
+                byte* imgPtr = (byte*)byteArray.Scan0;
+                for (int y = 0; y < Height; y++)
+                {
+                    for (int x = 0; x < Width; x++)
+                    {
+                        *imgPtr = (byte)ImgData[x + Height * y];       //B
+                        *(imgPtr + 1) = (byte)ImgData[x + Height * y]; //G 
+                        *(imgPtr + 2) = (byte)ImgData[x + Height * y]; //R  
+                        imgPtr += 3;
+                    }
+                    imgPtr += ByteOfSkip; // 跳過Padding bytes
+                }
+            }
+            myBitmap.UnlockBits(byteArray);
+            return myBitmap;
+        }
+
+        private void rGBtoGrayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int []f;
+            int []g;
+            foreach (MSForm cF in MdiChildren)
+			   {
+					if (cF.Focused)
+					{
+					    f = bmp2array(cF.pBitmap);
+			            g=new int[w*h];
+                        unsafe
+                        {
+                            fixed (int* f0 = f) fixed (int* g0 = g)
+                            {
+                                toGray(f0, w, h, g0);
+                            }
+                        } 
+                        NpBitmap = array2bmp(g);
+				        break;
+				    }
+			   }
+			MSForm childForm = new MSForm();
+	        childForm.MdiParent = this;
+            childForm.pf1 = stStripLabel;
+			childForm.pBitmap = NpBitmap; 
+			childForm.Show();
+        }
+
+        private void iPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        //圖片翻轉
+        private void verticalFlipToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int[] f;
+            int[] g;
+            foreach (MSForm cF in MdiChildren)
+            {
+                if (cF.Focused)
+                {
+                    f = bmp2array(cF.pBitmap);
+                    g = new int[w * h];
+                    unsafe
+                    {
+                        fixed (int* f0 = f) fixed (int* g0 = g)
+                        {
+                            Vertical_Flip(f0, w, h, g0);
+                        }
+                    }
+                    NpBitmap = array2bmp(g);
+                    break;
+                }
+            }
+            MSForm childForm = new MSForm();
+            childForm.MdiParent = this;
+            childForm.pf1 = stStripLabel;
+            childForm.pBitmap = NpBitmap;
+            childForm.Show();
+        }
+        private void degreeRotationToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            int[] f;
+            int[] g;
+            foreach (MSForm cF in MdiChildren)
+            {
+                if (cF.Focused)
+                {
+                    f = bmp2array(cF.pBitmap);
+                    g = new int[w * h];
+                    unsafe
+                    {
+                        fixed (int* f0 = f) fixed (int* g0 = g)
+                        {
+                            Counterclockwise_Rotation(f0, w, h, g0);
+                        }
+                    }
+                    NpBitmap = array2bmp(g);
+                    break;
+                }
+            }
+            MSForm childForm = new MSForm();
+            childForm.MdiParent = this;
+            childForm.pf1 = stStripLabel;
+            childForm.pBitmap = NpBitmap;
+            childForm.Show();
+        }
+        private void degreeRotationToolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            int[] f;
+            int[] g;
+            foreach (MSForm cF in MdiChildren)
+            {
+                if (cF.Focused)
+                {
+                    f = bmp2array(cF.pBitmap);
+                    g = new int[w * h];
+                    unsafe
+                    {
+                        fixed (int* f0 = f) fixed (int* g0 = g)
+                        {
+                            clockwise_Rotation(f0, w, h, g0);
+                        }
+                    }
+                    NpBitmap = array2bmp(g);
+                    break;
+                }
+            }
+            MSForm childForm = new MSForm();
+            childForm.MdiParent = this;
+            childForm.pf1 = stStripLabel;
+            childForm.pBitmap = NpBitmap;
+            childForm.Show();
+        }
+        private void horizontalFlipToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int[] f;
+            int[] g;
+            foreach (MSForm cF in MdiChildren)
+            {
+                if (cF.Focused)
+                {
+                    f = bmp2array(cF.pBitmap);
+                    g = new int[w * h];
+                    unsafe
+                    {
+                        fixed (int* f0 = f) fixed (int* g0 = g)
+                        {
+                            Horizontal_Flip(f0, w, h, g0);
+                        }
+                    }
+                    NpBitmap = array2bmp(g);
+                    break;
+                }
+            }
+            MSForm childForm = new MSForm();
+            childForm.MdiParent = this;
+            childForm.pf1 = stStripLabel;
+            childForm.pBitmap = NpBitmap;
+            childForm.Show();
+        }
+
+
+        //直線圖
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
